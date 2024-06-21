362c31d926967735ed9762b053dff678d4cd21be
Viveklagishetty <Viveklagishetty@gmail.com>
2024-06-21 02:18:56 +0000

    first commit

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const NotFound = () => (
+  <>
+    <Header />
+    <div className="products-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="product-img"
+      />
+    </div>
+  </>
+)
+
+export default NotFound
